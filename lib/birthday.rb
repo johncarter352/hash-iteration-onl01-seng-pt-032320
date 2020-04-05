ffe914cd2858feee9cefa96e0db661ce2189537d
John Carter <johncarter352@gmail.com>
2020-04-05 07:42:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ def age_appropriate_birthday(array)
     if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   elsif
-    "You are too old for this."
+    puts "You are too old for this."
   end
 end
 end
