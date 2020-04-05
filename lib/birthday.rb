5957c117d2c8401bd1e9be31a0083b72d206ef8e
John Carter <johncarter352@gmail.com>
2020-04-05 07:41:34 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 
 def age_appropriate_birthday(array)
   array.each do |kids_name, age|
-    if #{age} <= 12
+    if #{age} < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else
     "You are too old for this."
